9ee42c6a19f205449de3664acaed3ecc54432081
Rayyan Sarkhot <rayyanrsarkhot@gmail.com>
2023-11-09 14:30:44 -0500

    Added stuff

--- wordCounter.c
+++ wordCounter.c
@@ -1,20 +1,84 @@
 #include <stdio.h>
 #include <sys/stat.h>
+#include <string.h>
+#include <dirent.h>
+#include <sys/stat.h>
+// #include <vcruntime.h>
+
+struct stat fileStat;
+struct dirent *currFile;
+
+void fileReader(char *path) {
+
+    printf("%s is a file.\n", path);
+    
+}
+
+void recursiveFind(char *dPath) {
+    
+    // Opens current directory
+    DIR *dir;
+    dir = opendir(dPath);
+    
+    if (dir == NULL) {    
+        perror("Error opening directory");
+    } else {
+    
+        // Reads a file from directory
+        currFile = readdir(dir); 
+        while(currFile != NULL) {
+
+            // Checks if directory name is to deeper or higher and ignores this directory entry
+            if (strcmp(currFile->d_name, ".") == 0 || strcmp(currFile->d_name, "..") == 0) {
+                currFile = readdir(dir);
+                continue;
+            }
+
+            // Constructs full directory path
+            char path[PATH_MAX];
+            snprintf(path, sizeof(path), "%s/%s", dPath, currFile->d_name);
+
+            // Check if current file is a txt file
+            if (stat(path, &fileStat) == 0) {
+                
+                if (S_ISDIR(fileStat.st_mode)) {
+                    
+                    printf("%s is a directory.\n", currFile->d_name);
+                    recursiveFind(path);
+
+                } else {
+                    // printf("%s is not a directory.\n", currFile->d_name);
+                    fileReader(path);
+                }
+                currFile = readdir(dir);
+
+            } else {
+                perror("stat");
+            }
+
+        }
+
+    }
+    
+    closedir(dir);
+
+}
 
 void findTextFiles(int argc, char *argv[]) {
-
-    struct stat fileStat;
 
     // While there are files to be checked
     while(argc > 0){
 
         // Check if current file is a txt file
-        if (stat(argv[1], &fileStat) == 0) {
+        if (stat(argv[argc], &fileStat) == 0) {
             
             if (S_ISDIR(fileStat.st_mode)) {
-                printf("%s is a directory.\n", argv[1]);
+                printf("%s is a directory.\n", argv[argc]);
+                char *dPath = argv[argc];
+                recursiveFind(dPath);
+
             } else {
-                printf("%s is not a directory.\n", argv[1]);
+                printf("%s is a file.\n", argv[argc]);
             }
 
         } else {
@@ -22,8 +86,8 @@ void findTextFiles(int argc, char *argv[]) {
             
         }
 
-        break;
-        
+        --argc; 
+
     }
 
 }
